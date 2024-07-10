2a5d5a83c998092f55c8e817ed10bdc084c18694
Alex <alejandro.gomez.garcia@udc.es>
2024-07-10 21:39:23 +0200

    Commit inicial

---
+++ TFG-APP-Backend/src/main/java/es/udc/tfg/app/TfgAppBackendApplication.java
@@ -0,0 +1,14 @@
+package es.udc.tfg.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgAppBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TfgAppBackendApplication.class, args);
+	}
+
+}
---
+++ TFG-APP-Backend/src/test/java/es/udc/tfg/app/TfgAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.udc.tfg.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
